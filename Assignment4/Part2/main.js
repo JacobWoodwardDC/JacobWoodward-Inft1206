75d8b7836d764178b11f2e97bc0213246fd09106
JacobWoodwardDC <jacob.woodward@dcmail.ca>
2025-03-24 15:04:11 -0400

    Update main.js

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -10,13 +10,13 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-const images = [`pic1.jpg`, `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const images = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  pic1.jpg : 'Closeup of a human eye',
+  pic2.jpg : 'Rock that looks like a wave',
+  pic3.jpg : 'Purple and white pansies',
+  pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  pic5.jpg : 'Large moth on a leaf'
 }
 
 for (const image of images) {
