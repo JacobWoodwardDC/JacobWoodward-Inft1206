11d0b6bd0bf49e1db7bfa33721e52d12e7924a9d
JacobWoodwardDC <jacob.woodward@dcmail.ca>
2025-03-25 11:57:07 -0400

    Add files via upload

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -3,15 +3,15 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-    const random = Math.floor(Math.random()*array.length);
+    const random = Math.floor(Math.random() * array.length);
     return array[random];
 }
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertx = ["Willy the Goblin","Big Daddy","Father Christmas"];
-const inserty = ["the soup kitchen","Disneyland","The White House"];
-const insertz = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const inserty = ["the soup kitchen", "Disneyland", "The White House"];
+const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
@@ -24,21 +24,22 @@ function result() {
 
     newStory = newStory
         .replaceAll(":insertx:", xItem)
-        .replace(":inserty:", yItem)
-        .replace(":insertz:", zItem)
-    
+        .replaceAll(":inserty:", yItem)
+        .replaceAll(":insertz:", zItem);
+
     if(customName.value !== '') {
         const name = customName.value;
-        newStory = newStory.replace("Bob",name);
+        newStory = newStory.replace("Bob", name);
     }
 
     if(document.getElementById("uk").checked) {
-        const weight = `${Math.round(300/14)} stone`;
-        const temperature = `${Math.round((94-32)*(5/9))} centigrade`;
+        const weight = `${Math.round(300 / 14)} stone`;
+        const temperature = `${Math.round((94 - 32) * (5 / 9))} centigrade`;
         newStory = newStory
             .replace("300 pounds", weight)
-            .replace("94 fahrenheit", temperature)
+            .replace("94 fahrenheit", temperature);
     }
+
     story.textContent = newStory;
-    story.computedStyleMap.visibiity = 'visible';
+    story.style.visibility = 'visible';
 }
